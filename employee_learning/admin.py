058872e74ed23a09fc27149b79d15e671643277b
cuongduong2710 <cuongduongngocquoc@gmail.com>
2024-11-15 11:31:44 +0700

    create LearningCourse model ManyToMany with Employee

--- employee_learning/admin.py
+++ employee_learning/admin.py
@@ -4,7 +4,9 @@ from django.contrib import admin
 from .models import Employee
 from .models import Division
 from .models import PersonalInfo
+from .models import LearningCourse
 
 admin.site.register(Employee)
 admin.site.register(Division)
 admin.site.register(PersonalInfo)
+admin.site.register(LearningCourse)
